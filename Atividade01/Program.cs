3289143e0628b6b4fd86e7ee0db1a30c6245c298
Desenvolvedor <Desenvolvedor@P2200042332>
2023-09-26 18:57:20 -0300

    Add project files.

---
+++ Atividade01/Program.cs
@@ -0,0 +1,44 @@
+﻿// Faça um programa que leia três números, verifique (usando if e else) 
+//e mostre o maior e o menor deles;  (Não utilize estrutura de repetição)
+int numero1, numero2, numero3, maiorNum = 0, menorNum = 0;
+
+Console.WriteLine("Digite o primeiro numero");
+numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo numero");
+numero2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro numero");
+numero3 = Convert.ToInt32(Console.ReadLine());
+
+if (numero1 > numero2 && numero3 < numero2)
+{
+    maiorNum = numero1;
+
+
+}
+if (numero2 > numero3 && numero1 < numero2)
+
+{
+    maiorNum = numero2;
+}
+if (numero3 > numero2 && numero1 < numero3)
+{
+    maiorNum = numero3;
+}
+else if (numero1 < numero2 && numero3 > numero2)
+{
+    menorNum = numero1;
+}
+if (numero2 < numero3 && numero1 > numero2)
+{
+    menorNum = numero2;
+}
+if (numero3 < numero2 && numero2 > numero3)
+{
+    menorNum = numero3;
+}
+
+
+
+Console.WriteLine("Maior numero e " + maiorNum + " e o menor numero e " + menorNum);
